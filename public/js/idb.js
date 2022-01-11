6d82739d8f14d17481d74513c9bc23a159c47c02
Anthony Gonzalez <shibby75@live.com>
2022-01-10 18:02:50 -0700

    finished idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -19,3 +19,51 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
   console.log(event.target.errorCode);
 };
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+
+  budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+
+  const getAll = budgetObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+
+          const budgetObjectStore = transaction.objectStore("new_transaction");
+
+          budgetObjectStore.clear();
+
+          alert("All the transactions has been submitted.");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadTransaction);
